ab5f6215441554e8beca2856c2921e70057501d4
Alexey Aslamazov <aslamazov2005@gmail.com>
2020-12-05 16:39:13 +0300

    Add project files.

---
+++ HelloWorldSkillBox.cpp
@@ -0,0 +1,36 @@
+
+#include <iostream>
+
+//Вызывает функцию srd::count
+void print()
+{
+    //Потому что хотим использовать стандартную библиотеку для печати в консоль
+    // и не использовать какие либо другие бибилотеки.
+    std::cout << "Hello World!\n"; //This Function Prints
+
+}
+/*
+Присваиваем значение переменным и печатаем Hello Skillbox!
+*/
+int main()
+{
+    int x = 100;
+    3 + 7;
+    int y = x + 100;
+
+    int b = 0;
+    b = b + 2;
+
+    int test;
+    test = 100500;
+
+    int test2 = 1005001;
+
+
+
+    int mult = x * y;
+    int random = 1002;
+
+    std::cout << random;
+}
+
